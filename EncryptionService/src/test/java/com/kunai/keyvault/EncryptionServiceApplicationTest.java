08336a75dcc2c83101f3162b24eb36bf3f94a8c9
Aaron Cooley <acooley@kunaiconsulting.com>
2019-05-19 18:50:35 -0700

    Add heathcheck test.

--- EncryptionService/src/test/java/com/kunai/keyvault/EncryptionServiceApplicationTest.java
+++ EncryptionService/src/test/java/com/kunai/keyvault/EncryptionServiceApplicationTest.java
@@ -1,10 +1,14 @@
 package com.kunai.keyvault;
 
 import com.kunai.keyvault.config.EncryptionConfiguration;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,6 +22,12 @@ import static org.hamcrest.core.Is.is;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EncryptionServiceApplicationTest {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Autowired
     EncryptionConfiguration encryptionConfiguration;
 
@@ -29,5 +39,15 @@ public class EncryptionServiceApplicationTest {
     public void contextLoads() {
         assertThat("Encryption Key is in the configuration", encryptionConfiguration.getKey(), is("ABCDEF0123456789"));
         assertThat("Encryption Type is in the configuration", encryptionConfiguration.getType(), is("aes"));
+    }
+
+    /**
+     * Test to make sure that the health check is working
+     */
+    @Test
+    public void healthCheckEnabled() {
+        String responseBody = this.restTemplate.getForEntity("http://localhost:" + port + "/api/actuator/health", String.class).getBody();
+
+        Assert.assertThat(responseBody, CoreMatchers.is("{\"status\":\"UP\"}"));
     }
 }
